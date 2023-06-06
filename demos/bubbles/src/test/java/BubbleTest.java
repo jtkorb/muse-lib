030660ffbdb3ab3ef8e229365d5d7cd884d3d637
Tim Korb <jtkorb@bikmort.com>
2023-06-06 11:45:38 -0400

    Add more test infrastrucuture and template for Bubbles

---
+++ demos/bubbles/src/test/java/BubbleTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BubbleTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void succeeds() {
+        assertEquals(1, 1);
+    }
+}
