d2401028aab2f5904391689f647babf4989d5c64
Tim Korb <jtkorb@bikmort.com>
2025-02-11 15:25:15 -0500

    Remove unusued imports in test files

--- library/src/test/java/edu/purdue/jtk/PointVectorTest.java
+++ library/src/test/java/edu/purdue/jtk/PointVectorTest.java
@@ -1,10 +1,9 @@
 package edu.purdue.jtk;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PointVectorTest {
     PointVector pv1, pv2;
